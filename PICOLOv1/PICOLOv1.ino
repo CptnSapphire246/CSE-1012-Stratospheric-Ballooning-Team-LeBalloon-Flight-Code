0aec048b0cf371a1c90d22c2cefb307475c5503e
Nishanth Kavuru <136984735+CptnSapphire246@users.noreply.github.com>
2025-06-09 13:16:29 +0530

    Manually set throttle and record thrust
    This modification to PICOLOv1 incorporates code from TestStand_Thrust and ESC_Controller which allows the flight computer to change the throttle percentage on the ESC motor through manual serial inputs. Both the throttle percentages and the applied thrust on the test stand are recorded in the table outputted to the serial monitor which can be read as a .csv file.

--- PICOLOv1/PICOLOv1.ino
+++ PICOLOv1/PICOLOv1.ino
@@ -2,8 +2,9 @@
 _____________________________________________________________
 Code for the PICOLO Flight Computer
 Code by: Radhakrishna Vojjala
-Date of last modification: 23 Jan 2025
-Version 1.0
+Modified by: Nishanth Kavuru
+Date of last modification: 2 Apr 2025
+Version 2.0
 _____________________________________________________________
 
 */
@@ -20,25 +21,98 @@ _____________________________________________________________
 #include "Thermistor.h"
 #include "Variables.h"
 #include "StemmaQtOLED.h" // Custom Library to control the StemmQT OLED screen
+#include "Adafruit_HX711.h"
+#include <Servo.h>
 
 #define GPS_RUN_RATE  2.0 // Max GPS update speed in Hz. May not update at this speed.
 #define DATA_RATE 1.0 // Max rate of data aqusition in Hz. Set to 100 or some huge number to remove the limiter
 #define VERSION "1.0"
+#define ESC_PIN 14  // ESC connected to GP14
 
 // Config variables.
+
+  // Define the pins for the HX711 communication
+const uint8_t DATA_PIN = 12;  // Can use any pins!
+const uint8_t CLOCK_PIN = 13; // Can use any pins!
+
+Adafruit_HX711 hx711(DATA_PIN, CLOCK_PIN);
+
+Servo esc;
+int throttle = 0;  // Stores throttle value
+bool motorOn = false;  // Motor state
 
 bool usingM8N = true; // true for M8N, false for M9N
 
 // File header. Edit to add columns for other sensors.
 
-String header = "hh:mm:ss,T(s),T(ms),Hz,Fix Type,PVT,Sats,Date,Time,Lat,Lon,Alt(Ft),Alt(M),HorizAccuracy(MM),VertAccuracy(MM),VertVel(Ft/S),VertVel(M/S),ECEFstat,ECEFX(M),ECEFY(M),ECEFZ(M),NedVelNorth(M/S),NedVelEast(M/S),NedVelDown(M/S),GndSpd(M/S),Head(Deg),PDOP,ExtT(F),ExtT(C),IntT(F),IntT(C),Pa,kPa,ATM,PSI,MSTemp(C),MSTemp(F),Alt SL Ft,Alt SL M,Alt Rel Ft,Alt Rel M,VertVel(ft/s),VertVel(m/s),Accel(x),Accel(y),Accel(z),Deg/S(x),Deg/S(y),Deg/S(z),Ori(x),Ori(y),Ori(z),Mag_T(x),Mag_T(y),Mag_T(z)z`,Thrust,Version:" + String(VERSION);
+String header = "hh:mm:ss,T(s),T(ms),Hz,Fix Type,PVT,Sats,Date,Time,Lat,Lon,Alt(Ft),Alt(M),HorizAccuracy(MM),VertAccuracy(MM),VertVel(Ft/S),VertVel(M/S),ECEFstat,ECEFX(M),ECEFY(M),ECEFZ(M),NedVelNorth(M/S),NedVelEast(M/S),NedVelDown(M/S),GndSpd(M/S),Head(Deg),PDOP,ExtT(F),ExtT(C),IntT(F),IntT(C),Pa,kPa,ATM,PSI,MSTemp(C),MSTemp(F),Alt SL Ft,Alt SL M,Alt Rel Ft,Alt Rel M,VertVel(ft/s),VertVel(m/s),Accel(x),Accel(y),Accel(z),Deg/S(x),Deg/S(y),Deg/S(z),Ori(x),Ori(y),Ori(z),Mag_T(x),Mag_T(y),Mag_T(z)z`, Thrust, Throttle, Version:" + String(VERSION);
 
 void setup() {
 
   systemSetup();
+
+
+  Serial.begin(115200);
+
+// Thrust Setup
+  // wait for serial port to connect. Needed for native USB port only
+  while (!Serial) {
+    delay(10);
+  }
+
+  Serial.println("Adafruit HX711 Test!");
+
+  // Initialize the HX711
+  hx711.begin();
+
+  // read and toss 3 values each
+  Serial.println("Tareing....");
+  for (uint8_t t=0; t<3; t++) {
+    hx711.tareA(hx711.readChannelRaw(CHAN_A_GAIN_128));
+    hx711.tareA(hx711.readChannelRaw(CHAN_A_GAIN_128));
+  }
+
+// ESC Setup
+  esc.attach(ESC_PIN);
+
+  Serial.println("Arming ESC...");
+  esc.writeMicroseconds(1000);  // Min throttle for arming
+  delay(3000);
+
+  Serial.println("Press 'S' once to switch on, type in your throttle, and press 'S' again to switch off");
 }
 
 void loop() {
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  // Serial Output Thrust Value
+  // Read from Channel A with Gain 128, can also try CHAN_A_GAIN_64 or CHAN_B_GAIN_32
+  // since the read is blocking this will not be more than 10 or 80 SPS (L or H switch)
+  int32_t weightA128 = hx711.readChannel(CHAN_A_GAIN_128);
+
+// Set Throttle and Serial Output Throttle Value
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();  // Remove extra spaces/newlines
+
+    int newThrottle = input.toInt();
+      if (newThrottle >= 0 && newThrottle <= 100) {
+        throttle = newThrottle;
+        Serial.print("Throttle set to ")
+        Serial.print(throttle);
+        Serial.println("%");
+      }
+
+    if (input.equalsIgnoreCase("S")) {
+      motorOn = true;;
+    } 
+  }
+
+  // Only send throttle if motor is ON
+  int pulse = motorOn ? map(throttle, 0, 100, 1000, 2000) : 1000;
+  esc.writeMicroseconds(pulse);
+
+  delay(100);
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
   if ((millis() - nowTimeMS) >= loopTime) {
     systemUpdate();
@@ -197,6 +271,12 @@ void loop() {
     data += ",";
     data += String(magnetometer[2]);
     data += ",";
+
+    data += String(weightA128);
+    data += ",";
+    data += String(throttle);
+    data += ",";
+
 
     /*
       data form additional sensors
